72309178bed155d90414bed84e5e22b9d26a58aa
Ozkar <o0z.kar42@gmail.com>
2012-12-11 10:18:51 +0100

    space, I need more space!

--- database/src/database/CheckURL.java
+++ database/src/database/CheckURL.java
@@ -9,21 +9,21 @@ import java.sql.Driver;
  * Time: 20:55
  * To change this template use File | Settings | File Templates.
  */
-public class CheckURL {
+public class CheckURL{
 
-	public CheckURL() {
-		try {
-			Driver driver = (Driver) Class.forName("symantex.itools.db.jdbc.Driver").newInstance();
-			// "org.postgresql.Driver").newInstance();
-			String dbURL = "jdbc:dbaw://localhost:8888/Uni";
-			if (driver.acceptsURL(dbURL)) {
-				System.out.println("URL is valid");
-			} else {
-				System.out.println("URL is not valid");
-			}
-		} catch (Exception e) {
-			System.err.println("FATAL ERROR: "+e.getMessage());
-		}
-	}
+  public CheckURL() {
+    try {
+      Driver driver = (Driver) Class.forName( "symantex.itools.db.jdbc.Driver" ).newInstance();
+      // "org.postgresql.Driver").newInstance();
+      String dbURL = "jdbc:postgresql://db.f4.htw-berlin.de";
+      if ( driver.acceptsURL( dbURL ) ) {
+        System.out.println( "URL is valid" );
+      } else {
+        System.out.println( "URL is not valid" );
+      }
+    } catch ( Exception e ) {
+      System.err.println( "FATAL ERROR: " + e.getMessage() );
+    }
+  }
 
 }
