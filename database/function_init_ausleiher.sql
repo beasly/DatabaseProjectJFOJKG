3dd68c3b50600d99e249e0054494ee83470b972c
Kevin <kevin.goy.90@gmail.com>
2013-01-24 12:17:25 +0100

    Doc

--- database/function_init_ausleiher.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION init_ausleiher()
-	RETURNS trigger AS $$
-	BEGIN
-		INSERT INTO ausgeliehenan VALUES ( NEW.isbn, 3, CURRENT_DATE );
-		RAISE NOTICE 'isbn = %', new.isbn;
-		RETURN new;
-	END;
-$$ language plpgsql;
-
--- database/trigger_insert_ausleiher.sql
+++
@@ -1,5 +0,0 @@
-create trigger init_ausleiher
-  after insert on buch
-	for each row
-	execute procedure init_ausleiher();
-
