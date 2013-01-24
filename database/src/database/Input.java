943a80e87683606171ec2463e732f31f9fe0bf62
jufi <jufert666@googlemail.com>
2013-01-24 10:36:10 +0100

    Added comments to Input.java

--- database/src/database/Input.java
+++ database/src/database/Input.java
@@ -1,14 +1,22 @@
 package database;
 
 /**
- * Created with IntelliJ IDEA.
- * User: jufert
- * Date: 10.01.13
- * Time: 22:11
- * To change this template use File | Settings | File Templates.
+ * The class <code> Input </code> contains methods to get and check input data.
+ *
+ * User: Julian Fink
+ * Date: 24.12.12
+ * Time: 22:22
+ *
  */
 public class Input {
 
+  /**
+   * Checks if a String is an Integer.
+   *
+   *
+   * @param s String Input
+   * @return Boolean value
+   */
   public static boolean isFloat(String s) {
     try
     {
